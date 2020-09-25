7ba51e681366283eb93547096ce97e7bdde88b48
浅倉麗子 <asakurareiko@protonmail.ch>
2020-09-24 20:06:51 -0400

    Refactor hooking and CMake script
    And change optimisation options.

--- main.c
+++ main.c
@@ -18,21 +18,44 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // 六花ＰＲＯＪＥＣＴ
 
 #include <string.h>
+
 #include <psp2kern/kernel/modulemgr.h>
 #include <psp2kern/power.h>
+
 #include <taihen.h>
+
+#define USED __attribute__ ((used))
+#define UNUSED __attribute__ ((unused))
 
 #define GLZ(x) do {\
 	if ((x) < 0) { goto fail; }\
 } while (0)
 
-#define HOOK_EXPORT(idx, mod, libnid, funcnid, func)\
-	(hook_id[idx] = taiHookFunctionExportForKernel(\
-		KERNEL_PID, hook_ref+idx, mod, libnid, funcnid, func##_hook))
-
 #define N_HOOK 1
 static SceUID hook_id[N_HOOK];
 static tai_hook_ref_t hook_ref[N_HOOK];
+
+static SceUID hook_export(int idx, char *mod, int libnid, int funcnid, void *func) {
+	SceUID ret = taiHookFunctionExportForKernel(KERNEL_PID, hook_ref+idx, mod, libnid, funcnid, func);
+	if (ret >= 0) {
+		hook_id[idx] = ret;
+	}
+	return ret;
+}
+#define HOOK_EXPORT(idx, mod, libnid, funcnid, func)\
+	hook_export(idx, mod, libnid, funcnid, func##_hook)
+
+static int UNHOOK(int idx) {
+	int ret = 0;
+	if (hook_id[idx] >= 0) {
+		ret = taiHookReleaseForKernel(hook_id[idx], hook_ref[idx]);
+		if (ret == 0) {
+			hook_id[idx] = -1;
+			hook_ref[idx] = -1;
+		}
+	}
+	return ret;
+}
 
 static int kscePowerSetIdleTimer_hook(int callback_slot, SceUInt64 time) {
 	time = callback_slot == 1 ? 0 : time;
@@ -45,13 +68,10 @@ static void startup(void) {
 }
 
 static void cleanup(void) {
-	for (int i = 0; i < N_HOOK; i++) {
-		if (hook_id[i] >= 0) { taiHookReleaseForKernel(hook_id[i], hook_ref[i]); }
-	}
+	for (int i = 0; i < N_HOOK; i++) { UNHOOK(i); }
 }
 
-int _start() __attribute__ ((weak, alias("module_start")));
-int module_start(SceSize argc, const void *argv) { (void)argc; (void)argv;
+USED int module_start(UNUSED SceSize args, UNUSED const void *argp) {
 	startup();
 	GLZ(HOOK_EXPORT(0, "ScePower", 0x1590166F, 0x6BC26FC7, kscePowerSetIdleTimer));
 	kscePowerSetIdleTimer(1, 0);
@@ -62,7 +82,7 @@ fail:
 	return SCE_KERNEL_START_FAILED;
 }
 
-int module_stop(SceSize argc, const void *argv) { (void)argc; (void)argv;
+USED int module_stop(UNUSED SceSize args, UNUSED const void *argp) {
 	cleanup();
 	return SCE_KERNEL_STOP_SUCCESS;
 }
